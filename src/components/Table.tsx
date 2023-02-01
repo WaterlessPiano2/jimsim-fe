3ac76e54be0c3b5843f2aa65cf3fc41596989b03
salih <scderya@gmail.com>
2023-02-01 14:22:40 +0000

    added react-table

---
+++ src/components/Table.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { useTable } from 'react-table'
+ 
+ function Table() {
+   const data = React.useMemo(
+     () => [
+       {
+         col1: 'Hello',
+         col2: 'World',
+       },
+       {
+         col1: 'react-table',
+         col2: 'rocks',
+       },
+       {
+         col1: 'whatever',
+         col2: 'you want',
+       },
+     ],
+     []
+   )
+   
+   const columns = React.useMemo(
+     () => [
+       {
+         Header: 'Column 1',
+         accessor: 'col1' as const, // accessor is the "key" in the data
+        },
+       {
+         Header: 'Column 2',
+         accessor: 'col2' as const,
+       },
+     ],
+     []
+   )
+ 
+   const {
+     getTableProps,
+     getTableBodyProps,
+     headerGroups,
+     rows,
+     prepareRow,
+   } = useTable({ columns, data })
+ 
+  
+   return (
+     <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+      <thead>
+        {headerGroups.map((headerGroup) => {
+          const { key, ...restHeaderGroupProps } =
+            headerGroup.getHeaderGroupProps();
+          return (
+            <tr key={key} {...restHeaderGroupProps}>
+              {headerGroup.headers.map((column) => {
+                const { key, ...restColumn } = column.getHeaderProps();
+                return (
+                  <th key={key} {...restColumn}>
+                    {column.render("Header")}
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </thead>
+      <tbody {...getTableBodyProps}>
+        {rows.map((row) => {
+          prepareRow(row);
+          const { key, ...restRowProps } = row.getRowProps();
+          return (
+            <tr key={key} {...restRowProps}>
+              {row.cells.map((cell) => {
+                const { key, ...restCellProps } = cell.getCellProps();
+                return (
+                  <td key={key} {...restCellProps}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+     </table>
+   )
+ }
+
+ export default Table;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Table from '@/components/Table'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
@@ -40,23 +41,8 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <Table/>
+
         </div>
 
         <div className={styles.grid}>
