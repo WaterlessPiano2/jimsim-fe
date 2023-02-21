830462873e93716121620fdaa6735047f7cbc4da
salih <scderya@gmail.com>
2023-02-21 00:04:54 +0000

    added the first compoenent - graph to the expanded version

--- src/components/RowExpansion.tsx
+++ src/components/RowExpansion.tsx
@@ -1,5 +1,12 @@
+import styles from "@/styles/Home.module.css";
+import ExpansionGraph from "./ExpansionGraph";
+
 const RowExpansion = () => {
-  return <span>Expanded</span>;
+  return (
+    <div className={styles.rowExpansion}>
+      <ExpansionGraph data="uv" />
+    </div>
+  );
 };
 
 export default RowExpansion;
