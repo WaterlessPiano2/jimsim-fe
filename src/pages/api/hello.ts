15211f310824d6ea0d1bfa64ef3ce268deb41161
salih <scderya@gmail.com>
2023-03-11 01:21:31 +0000

    Installed Prisma, updated build script to generate the db schema in the front end and pull it in to node modules, added fist prisma script to fetch data from db, and respond with it from the GET /hello endpoint.

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
 
-type Data = {
-  name: string
-}
+const prisma = new PrismaClient()
 
-export default function handler(
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+const data = await prisma.defi_product_master.findMany({where:{staus:'active'}, orderBy:{product_id:'asc'}})
+
+  res.status(200).json(data)
+
 }
